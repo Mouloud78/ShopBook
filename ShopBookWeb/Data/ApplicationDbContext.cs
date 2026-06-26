b651735c17bce130f631110b1f297c3659fa0cb7
Mouloud78 <mouloudouldali78@gmail.com>
2026-06-25 21:36:19 -0400

    DbContext created

---
+++ ShopBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopBookWeb.Models;
+
+namespace ShopBookWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
