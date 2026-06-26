66d5486d142d26b3ffa22d67fb618fdf53dab4e0
Mouloud78 <mouloudouldali78@gmail.com>
2026-06-25 22:15:02 -0400

    category controller added

---
+++ ShopBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- ShopBookWeb/Data/ApplicationDbContext.cs
+++ ShopBookWeb/Data/ApplicationDbContext.cs
@@ -8,5 +8,16 @@ namespace ShopBookWeb.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id=1, Name="Action" },
+                new Category { Id = 2, Name = "ASciFi" },
+                new Category { Id = 3, Name = "History" }
+
+                );
+        }
     }
 }
